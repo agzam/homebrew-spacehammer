feacbdbecf053cdaa8be35cfdd1dd167844d7fdb
Ag Ibragimov <agzam.ibragimov@gmail.com>
2026-01-07 14:30:25 -0600

    update to 1.6.1

--- Casks/spacehammer.rb
+++ Casks/spacehammer.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 cask "spacehammer" do
-  version "1.6.0"
-  sha256 "ccc6d70e296b50a8c8ff461a636eba49d0a1085a58ff4c53c26a8186a548fe27"
+  version "1.6.1"
+  sha256 "e7c8c79c074384855596ca0a4483c4de908e62f6b5dc2d2b5a08983760e8e042"
 
   url "https://github.com/agzam/spacehammer/archive/refs/tags/#{version}.tar.gz"
   name "Spacehammer"
